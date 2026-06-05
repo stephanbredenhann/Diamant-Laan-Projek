7b8771a4c94283a14d372f789ec5b8c28c762a36
stephanbredenhann <144379630+stephanbredenhann@users.noreply.github.com>
2026-06-05 11:39:51 +0200

    feat: configure CORS, static files, Angular build target

--- src/DiamantLaan.Api/Program.cs
+++ src/DiamantLaan.Api/Program.cs
@@ -1,4 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS for Angular dev server
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevCors", policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
-app.MapGet("/", () => "Diamant Laan API");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("DevCors");
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
 app.Run();
